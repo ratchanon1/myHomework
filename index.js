cde75efabb434d98068b0758060f2317fe49e4bc
ratchanon1 <ozmosiszer@gmail.com>
2014-11-12 18:55:02 +0700

    create form to insert book in webapp

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var mongojs = require('mongojs')
 var app = express()
-
+var bodyParser = require('body-parser')
 var db = mongojs('my_server' , ['book']);
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.json())
 
 app.get('/api/book', function(req,res){
 	db.book.find({},function(err,books){
@@ -12,6 +12,12 @@ app.get('/api/book', function(req,res){
 	});
 })
 
+app.post('/api/book',function(req,res){
+	db.book.insert((req.body),function(err,books){
+		res.send(books);
+	});
+	
+});
 
 var server = app.listen(3000, function () {
   console.log('server is running')
