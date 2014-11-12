f0e072d3240339ac36a6632405b485ff6a67aac1
ratchanon1 <ozmosiszer@gmail.com>
2014-11-12 17:46:27 +0700

    using mongodb

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var express = require('express')
+var mongojs = require('mongojs')
 var app = express()
 
+var db = mongojs('my_server' , ['book']);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api/book', function(req,res){
-	var books = [
-		{title: 'CSS', price: 500},
-		{title: 'HTML', price: 1300},
-		{title: 'JSON', price: 4000}
 
-	];
-	res.send(books);
+app.get('/api/book', function(req,res){
+	db.book.find({},function(err,books){
+		res.send(books);
+	});
 })
+
+
 var server = app.listen(3000, function () {
   console.log('server is running')
 
